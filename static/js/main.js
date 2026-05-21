a184038f2f47a7570f9f124a0bc5c17922363269
Abdul Barik <amsbarik24@gmail.com>
2026-05-21 14:34:09 +0600

    FAQ section design updated and done

--- static/js/main.js
+++ static/js/main.js
@@ -112,3 +112,102 @@ showSlide(0);
 
 
 ///////////////// Hero Slider Js end
+
+
+
+
+////////////////////////////////////////////////////////////
+// FAQ js start 
+
+const faqItems=document.querySelectorAll('.faq-item');
+
+function openItem(item){
+
+    const content=item.querySelector('.faq-content');
+    const plus=item.querySelector('.icon-plus');
+    const minus=item.querySelector('.icon-minus');
+    const icon=item.querySelector('.icon-wrap');
+    const question=item.querySelector('.faq-question');
+
+    item.classList.add('active');
+
+    content.style.height=content.scrollHeight+'px';
+    content.classList.remove('opacity-0');
+    content.classList.add('opacity-100');
+
+    plus.classList.add('hidden');
+    minus.classList.remove('hidden');
+
+    // icon.classList.add('rotate-180');
+
+    question.classList.add('text-primary');
+
+}
+
+function closeItem(item){
+
+    const content=item.querySelector('.faq-content');
+    const plus=item.querySelector('.icon-plus');
+    const minus=item.querySelector('.icon-minus');
+    const icon=item.querySelector('.icon-wrap');
+    const question=item.querySelector('.faq-question');
+
+    item.classList.remove('active');
+
+    content.style.height='0px';
+    content.classList.remove('opacity-100');
+    content.classList.add('opacity-0');
+
+    plus.classList.remove('hidden');
+    minus.classList.add('hidden');
+
+    // icon.classList.remove('rotate-180'); 
+
+    question.classList.remove('text-primary');
+
+}
+
+
+window.addEventListener('load',()=>{
+
+    const first=document.querySelector('.faq-item.active');
+
+    openItem(first);
+
+});
+
+
+faqItems.forEach(item=>{
+
+    item.querySelector('.faq-btn')
+    .addEventListener('click',()=>{
+
+        const isActive=item.classList.contains('active');
+
+        faqItems.forEach(faq=>{
+
+            if(faq!==item){
+
+            closeItem(faq);
+
+            }
+
+        });
+
+        if(isActive){
+
+            closeItem(item);
+
+        }else{
+
+            openItem(item);
+
+        }
+
+    });
+
+});
+
+
+// FAQ js end 
+///////////////////////////////////////////////////////
